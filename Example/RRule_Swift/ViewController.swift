70475e2983ffdbaeeed9d00f67e76720bda260f7
Maged Mohammed <maged.mo.it@gmail.com>
2022-11-28 19:08:38 +0200

    add RRuleSwift source files

--- Example/RRule_Swift/ViewController.swift
+++ Example/RRule_Swift/ViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import RRule_Swift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let ruleString = "RRULE:FREQ=MONTHLY;DTSTART=20160404T021000Z;COUNT=5;INTERVAL=2;WKST=MO;BYDAY=MO,TU"
+        let rule = RecurrenceRule(rruleString: ruleString)
+        print(rule)
+        print(rule?.allOccurrences())
         // Do any additional setup after loading the view, typically from a nib.
     }
 
